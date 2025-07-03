bea87e7efa6aeac71e42517d288b23abc86d3289
steve2700 <stevnyaru@github.com>
2025-07-03 21:13:59 +0200

    solving the 401 error and restructure the frontend

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # CORS Middleware (Update allow_origins in production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YYmzXDYctdhpAUbSiW0FhHQB1V9nt7YbHjR0WUZia4g=\"\n}"
